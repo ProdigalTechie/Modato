d07d70b8635668f7c4a80672cc62c377bc486d37
ProdigalTechie <142155266+ProdigalTechie@users.noreply.github.com>
2024-12-12 19:43:14 -0600

    Update to add infinite piggy bank and harvesting (#8)
    * Fix broken version
    
    * Minor Stat UI improvements and infinite harvesting and piggy bank
    
    * Minor Stat UI improvements and infinite harvesting and piggy bank

--- mods-unpacked/ProdigalTechie-Modato/mod_main.gd
+++ mods-unpacked/ProdigalTechie-Modato/mod_main.gd
@@ -4,16 +4,24 @@ const MOD_ID = "ProdigalTechie-Modato"
 const MOD_DIR = MOD_ID + "/"
 
 const EXTS = [
+	"main.gd",
 	"ui/menus/run/character_selection.gd"
+]
+
+const TRANS = [
+	"translations.en.translation"
 ]
 
 func _init():
 	var dir = ModLoaderMod.get_unpacked_dir() + MOD_DIR
 	var ext_dir = dir + "extensions/"
-	ModLoaderLog.info("init", MOD_ID + ":Main")
+	var res_dir = dir + "translations/"
+	ModLoaderLog.info("installing script extensions", MOD_ID + ":Main")
 	for ext_path in EXTS:
-		ModLoaderLog.info("installing " + ext_path, MOD_ID + ":Main")
 		ModLoaderMod.install_script_extension(ext_dir + ext_path)
+	ModLoaderLog.info("installing translations", MOD_ID + ":Main")
+	for tr_path in TRANS:
+		ModLoaderMod.add_translation(res_dir + tr_path)
 
 func _ready():
 	ModLoaderLog.info("ready", MOD_ID + ":Main")
